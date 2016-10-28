db22d4eab4e395635012674ae1b78dc54209302b
maxline <sergml@mail.ru>
2016-10-28 14:01:44 +0300

    v02. before @autowired

---
+++ src/test/java/ua/rd/pizzaservice/SpringAppRunner.java
@@ -0,0 +1,45 @@
+package ua.rd.pizzaservice;
+
+/**
+ * @author Serhii_Mykhliuk
+ */
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.rd.pizzaservice.domain.Order;
+import ua.rd.pizzaservice.repository.OrderRepository;
+import ua.rd.pizzaservice.repository.PizzaRepository;
+import ua.rd.pizzaservice.services.OrderService;
+
+import java.util.Arrays;
+
+public class SpringAppRunner {
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext repoContext = new ClassPathXmlApplicationContext("repoContext.xml");  //запускает создание контейнера и считывает все бины
+        System.out.println(Arrays.toString(repoContext.getBeanDefinitionNames()));
+
+        //ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");  //запускает создание контейнера и считывает все бины
+
+
+        ConfigurableApplicationContext appContext = new ClassPathXmlApplicationContext(
+                                new String[]{"appContext.xml"}, repoContext);
+
+        System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+
+        PizzaRepository pizzaRepository = (PizzaRepository) repoContext.getBean("pizzaRepository"); //имя плюс тип тогда не нужно будет делать каст
+        System.out.println(pizzaRepository.find(1));  // null т.к. не вызвался метод init
+
+
+        OrderRepository orderRepository = (OrderRepository) repoContext.getBean("orderRepository");
+        //System.out.println(orderRepository.find());
+
+        OrderService orderService = (OrderService) appContext.getBean("orderService");
+        Order order = orderService.placeNewOrder(null, 1, 3);
+        System.out.println(order);
+
+
+        appContext.close();
+        repoContext.close();
+
+    }
+}
